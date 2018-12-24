e309c1f84b1d26083e3cdcccb686f5f39159345a
hanmanthreddygurrala <46065120+hanmanthreddygurrala@users.noreply.github.com>
2018-12-23 21:05:32 -0800

    added size parameterized constructor(arraylist.java) -- by hamanth

--- src/main/java/com/hany/gittutorials/ds/arraylist/ArrayList.java
+++ src/main/java/com/hany/gittutorials/ds/arraylist/ArrayList.java
@@ -2,7 +2,14 @@ package com.hany.gittutorials.ds.arraylist;
 
 public class ArrayList {
 
+	private static final int DEFAULTSIZE = 10;
+	private int[] array=null;
+	
     public ArrayList(){
-        //this is a default constructor.
+        array = new int[DEFAULTSIZE];
+    }
+    
+    public ArrayList(int size) {
+    	array = new int[size];
     }
 }
